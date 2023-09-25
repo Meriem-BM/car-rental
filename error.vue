dd7e2a03443b58b4550ac00f2214f4f3d4d2acc9
Meriem-BM <barhoumi.meriem1@gmail.com>
2023-09-25 21:42:30 +0100

    feat: 404 page

---
+++ error.vue
@@ -0,0 +1,25 @@
+<template>
+  <NuxtLayout name="404">
+    <div
+      class="absolute h-screen w-full text-gray-darker flex flex-col justify-center items-center"
+    >
+      <h1
+        class="text-6xl font-bold text-gray-900 border-r-2 border-gray-900 pr-4"
+      >
+        404
+      </h1>
+      <p class="text-gray-700 border-b border-gray-700">Page not found</p>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script>
+export default {
+  name: "404",
+  methods: {
+    goBack() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
